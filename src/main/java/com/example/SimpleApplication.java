40bc190b2b6697b9c41d9e09445c3e0a63195d54
Blake Faris <blake.faris@bluestembrands.com>
2016-12-12 11:43:01 -0600

    Added a basic /hello endpoint.

--- src/main/java/com/example/SimpleApplication.java
+++ src/main/java/com/example/SimpleApplication.java
@@ -2,11 +2,24 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SimpleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
+    }
+
+    @RestController
+    protected static class HelloController {
+
+        @RequestMapping({"/hello"})
+        public @ResponseBody String sayHello() {
+            return "Hello World";
+        }
+    }
+
 }
